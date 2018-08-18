0e77e1033180389df9b3bf272941aebf78d33118
Sriharsha Damarla <32672504+sritar99@users.noreply.github.com>
2018-08-18 18:54:54 +0530

    Node setup
    Node sessions

---
+++ IntroToNode/echo.js
@@ -0,0 +1,9 @@
+function echo(str,num){
+    while(num>0){
+        console.log(num +" "+str);
+        num--;
+    }
+}
+
+
+echo("sri",10);
---
+++ IntroToNode/grader.js
@@ -0,0 +1,13 @@
+var total=0;
+var avg=0;
+function average(arr){
+    var total=0;
+    for(var i=0;i<arr.length;i++){
+        total=total+arr;
+    }
+    console.log("total ="+" " +total);
+    var avg=total/arr.length;
+    console.log("average ="+" "+avg);
+}
+
+var arr =[90,98,89,100,100,86,94];
