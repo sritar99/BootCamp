a8a01b8c526023bca30d1182f076881fcc640691
Sriharsha Damarla <32672504+sritar99@users.noreply.github.com>
2018-08-18 18:56:29 +0530

    Npm
    Npm base

--- app.js
+++ app.js
@@ -1,26 +1,4 @@
-var bodyParser=require("body-parser"),
-mongoose      =require("mongoose"),
-express       =require("express"),
-
-app           =express();
-
-mongoose.connect("mongodb://Localhost/rest_full_blog_app");
-
-app.set("view engine","ejs");
-
-app.use(express.static("public"));
-
-app.use(bodyParser.urlencoded({extended:true}));
-
-var blogSchema=new mongoose.Schema({
-    title:String,
-    image:String,
-    body:String,
-    created:{type:Date ,default:Date.now}
-});
-
-var Blog=mongoose.model("Blog",blogSchema);
-
-app.listen(process.env.PORT,process.env.IP,function(){
-    console.log("server is running");
-})
+var cat =require("cat-me");
+var joke=require("knock-knock-jokes")
+console.log(cat());
+console.log(joke());
